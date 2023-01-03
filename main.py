a4759d37bc2a7fad576462b6bac00d647005f17d
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-01-03 16:03:22 -0600

    Test2

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ class ExtractBetween(AddOn):
         end = self.data.get('end')
         for document in self.get_documents():
             text_to_parse = document.full_text
-            extracted_text = re.search('%s(.*)%s' % (start, end), text_to_parse).group(1)
+            print(text_to_parse)
+            """ extracted_text = re.search('%s(.*)%s' % (start, end), text_to_parse).group(1)
             with open(f"{document.title}.txt",  'w') as file:
                 file.write(extracted_text)
         os.chdir('..')
@@ -23,6 +24,7 @@ class ExtractBetween(AddOn):
         self.upload_file(open("extract.zip"))
         self.set_message("Add-On run complete.")
         shutil.rmtree("./out", ignore_errors=False, onerror=None)
+        """
 
 if __name__ == "__main__":
     ExtractBetween().main()
