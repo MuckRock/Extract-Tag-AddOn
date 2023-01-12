70c08b72fe10c5c735c2608dd8322ed180ff23c1
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2023-01-12 15:27:32 -0600

    Removed references to file handling, moved key extraction to outside loop

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 from documentcloud.addon import AddOn
 from documentcloud.exceptions import APIError
 from requests.exceptions import RequestException
@@ -6,21 +5,19 @@ from requests.exceptions import RequestException
 class ExtractAndTag(AddOn):
     """Add-On that extracts text between a start and end string"""
     def main(self):
-        os.makedirs(os.path.dirname("./out/"), exist_ok=True)
-        os.chdir('out')
         self.set_message("Extracting text from documents...")
         start = self.data.get('start')
         end = self.data.get('end')
+        name_key = self.data.get("key_name")
         for document in self.get_documents():
             text_to_parse = document.full_text
             text_to_parse = text_to_parse.replace("\n", " ")
             start_char = text_to_parse.find(start) + len(start)
             end_char = text_to_parse.find(end)
             extracted_text = text_to_parse[start_char:end_char]
-            name_key = self.data.get("key_name")
             try:
                 document.data[name_key] = extracted_text
-                document.put()
+                document.save()
             except (APIError, RequestException) as exc:
                 print("Tagging Error on Document", str(exc))
                 print(document.title)
